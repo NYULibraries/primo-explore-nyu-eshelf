1f67ea692b0b0ff78ba834678dd6416ce34ea9bc
Elliott Grieco <elliott.grieco@gmail.com>
2018-04-09 13:01:00 -0400

    Improves comment

--- src/spec/nyuEshelf.controller.spec.js
+++ src/spec/nyuEshelf.controller.spec.js
@@ -23,7 +23,7 @@ describe('nyuEshelfController', () => {
       return config;
     });
 
-    // mocks http to do nothing to avoid warnings
+    // Mocks $http to do nothing to avoid warnings. Http request tests handled in services.
     const mockHttp = (request) => new Promise((resolve, reject) => {});
     $provide.service('$http', () => mockHttp);
 
