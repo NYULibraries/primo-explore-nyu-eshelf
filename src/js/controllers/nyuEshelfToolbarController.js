a01146142a9e9f30e24a269620c151f801a868d4
Elliott Grieco <elliott.grieco@gmail.com>
2018-04-16 18:06:31 -0400

    Adds toolbar config value

--- src/js/controllers/nyuEshelfToolbarController.js
+++ src/js/controllers/nyuEshelfToolbarController.js
@@ -3,6 +3,7 @@ export default function(config, $scope) {
   this.$onInit = () => {
     $scope.myEshelfButtonClasses = config.myEshelfButtonClasses;
     $scope.eshelfUrl = config.envConfig.eshelfBaseUrl + "/?institution=" + config.envConfig.institution;
+    $scope.toolbar = config.toolbar;
   };
 
   $scope.openEshelf = () => {
